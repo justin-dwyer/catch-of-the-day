bc15650fa265c9a8405c99a780fb4a2668c99730
Justin Dwyer <justindwyer6@gmail.com>
2020-04-04 19:01:25 -0700

    Built router and 404 page. Now seeing if I can figure out how to pass URL params to a component.

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,11 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
-  render() {
+  render(props) {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header tagline="Fresh Seafood Market" />
+          <Header tagline="Fresh Seafood Market" storeId={props.match.params.storeId} />
         </div>
         <Order />
         <Inventory />
---
+++ src/components/NotFound.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const NotFound = () => (
+    <header className="top">
+      <h1>Store
+        <span className="ofThe">
+          <span className="of">Is</span>
+          <span className="the">Not</span>
+        </span>
+        Here
+      </h1>
+      <h3 className="tagline">
+        <span>Try A Different Store</span>
+      </h3>
+    </header>
+);
+
+export default NotFound;
