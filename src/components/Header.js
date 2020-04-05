4a20a538d47a537fe654a026022840423c4a2673
Justin Dwyer <justindwyer6@gmail.com>
2020-04-04 17:47:59 -0700

    First couple components built out

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <>
+      <header className="top">
+        <h1>Catch O' The Day</h1>
+        <h3 className="tagline">
+          <span>Fresh Daily</span>
+        </h3>
+      </header>
+      </>
+    )
+  }
+}
+
+export default Header;
