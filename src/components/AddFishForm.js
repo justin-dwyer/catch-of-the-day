96b116e3a5acd0d0271eeb3cf59a4c748607f8d3
Justin Dwyer <justindwyer6@gmail.com>
2020-04-05 11:34:43 -0700

    Working on inventory form, about to start state.

---
+++ src/components/AddFishForm.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+class AddFishForm extends React.Component {
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+  statusRef = React.createRef();
+  descriptionRef = React.createRef();
+  imageRef = React.createRef();
+
+  createFish = event => {
+    // 1. stop form from submitting
+    event.preventDefault();
+    const fish = {
+      name: this.nameRef.current.value,
+      price: parseFloat(this.priceRef.current.value),
+      status: this.statusRef.current.value,
+      description: this.descriptionRef.current.value,
+      image: this.imageRef.current.value
+    };
+    console.log("MAKIN' THE FISHIES YUM YUM 🐟🐠🐡");
+    console.log(this.nameRef.current.value);
+    console.log(this);
+    console.log(fish);
+  }
+
+  render() {
+    return (
+      <form className="fish-edit" onSubmit={this.createFish}>
+        {/* Name, Price, Availability, Description, Image, Button */}
+        <input name="name" ref={this.nameRef} type="text" placeholder="Name" />
+        <input name="price" ref={this.priceRef} type="text" placeholder="Price" />
+        <select name="status" ref={this.statusRef}>
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out</option>
+        </select>
+        <textarea name="description" ref={this.descriptionRef} placeholder="Description" />
+        <input name="image" ref={this.imageRef} type="text" placeholder="Image" />
+        <button type="submit">+ Add Fish</button>
+      </form>
+    )
+  }
+}
+
+export default AddFishForm;
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,19 +2,15 @@ import React from 'react';
 import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
-
-  constructor() {
-    super();
-    this.goToStore = this.goToStore.bind(this);
-  }
-
   myInput = React.createRef();
-  goToStore(event) {
+
+  goToStore = event => {
     // 1. Stop form from submitting
     event.preventDefault();
     // 2. get text from input
-    console.log(this);
+    const storeName = this.myInput.current.value;
     // 3. change page to /store/input
+    this.props.history.push(`/store/${storeName}`)
   }
   render() {
     return (
