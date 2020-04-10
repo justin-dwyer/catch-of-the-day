fce7eb87dfcd6a20c77715fa7116e13a4fa826d1
Justin Dwyer <justindwyer6@gmail.com>
2020-04-09 23:20:45 -0700

    Bah. event.currentTarget was not intuitive to find. And I had my function initialized in the wrong place/way.

--- src/components/EditFishForm.js
+++ src/components/EditFishForm.js
@@ -1,23 +1,20 @@
 import React from 'react';
 
 class EditFishFrom extends React.Component {
+  handleChange = event => {
+    console.log(event.currentTarget.value);
+  }
   render() {
-    // Send the updated data to App to update state.
-    function handleChange(event) {
-      console.log(event);
-      // this.props.fish[event]
-      this.props.updateFish();
-    }
     return (
       <div className="fish-edit">
-        <input type="text" name="name" value={this.props.fish.name} onChange={event => this.handleChange.bind(event)}/>
-        <input type="text" name="price" value={this.props.fish.price}/>
-        <select type="text" name="status" value={this.props.fish.status}>
+        <input type="text" name="name" value={this.props.fish.name} onChange={this.handleChange}/>
+        <input type="text" name="price" value={this.props.fish.price} onChange={this.handleChange}/>
+        <select type="text" name="status" value={this.props.fish.status} onChange={this.handleChange}>
           <option value="available">Fresh!</option>
           <option value="unavailable">Sold Out!</option>
         </select>
-        <textarea name="description" value={this.props.fish.description}/>
-        <input type="text" name="image" value={this.props.fish.image}/>
+        <textarea name="description" value={this.props.fish.description} onChange={this.handleChange}/>
+        <input type="text" name="image" value={this.props.fish.image} onChange={this.handleChange}/>
       </div>
     )
   }
